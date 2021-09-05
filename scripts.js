51e2f8baebd9f0c0a53ee912c7bb9597f6781583
Michael Vincent <mvincent1212@utexas.edu>
2021-09-05 19:50:37 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,62 @@
+const crashRide = document.getElementById("crash-ride");
+const hiHatTop = document.getElementById("hihat-top");
+
+function playSound (drum) {
+    const keyCode = drum.keyCode;
+    const keyDrum = document.querySelector(`div[data-key="${keyCode}"]`);
+
+    if(!keyDrum) return;
+
+    const audioForDrum = document.querySelector(`audio[data-key="${keyCode}"]`);
+    audioForDrum.currentTime = 0;
+    audioForDrum.play();
+
+    switch(keyCode) {
+        case 69:
+        case 82:
+            animateCrashOrRide();
+            break;
+        case 75:
+            animateHiHatClose();
+            break;
+    }
+
+    keyDrum.classList.add('playing');
+};
+
+function animateCrashOrRide () {
+    crashRide.style.transform = 'rotate(0deg) scale(1.5)';   
+};
+
+function animateHiHatClose () {
+    hiHatTop.style.top = '171px';
+};
+
+function removeRideTransition (drum) {
+    if(drum.propertyName !== 'transform') return;
+
+    drum.target.style.transform = 'rotate(-7.2deg) scale(1.5)';
+};
+
+function removeHiHatTransition (drum) {
+    if(drum.propertyName !== 'transform') return;
+
+    drum.target.style.top = '166px';
+};
+
+function removeKeyTransition (drum) {
+    if(drum.propertyName !== 'transform') return;
+
+    drum.target.classList.remove('playing');
+};
+
+const drumKeys = Array.from(document.querySelectorAll('.key'));
+
+drumKeys.forEach(key => {
+    key.addEventListener('transitionend', removeKeyTransition);
+});
+
+crashRide.addEventListener('transitionend', removeCrashRideTransition);
+hiHatTop.addEventListener('transitionend', removeHiHatTopTransition);
+
+Window.addEventListener('keydown', playSound);
